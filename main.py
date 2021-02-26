a2517a4a87046c7321970e106e03cc95256b0516
Spencer Briguglio <brigugls@uwindsor.ca>
2021-02-25 19:02:35 -0500

    A-Priori completed method completed.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import linecache
 import random
+from tqdm import tqdm
 import numpy as np
 
 
@@ -16,6 +17,8 @@ class APriori:
         self.selected_baskets = []
         self.freq_item_table = []
         self.pairs_tri_matrix = []
+        self.freq_pairs_list = []
+        self.runtime = 0
 
         self.open_file()
         self.set_n_buckets()
@@ -63,9 +66,8 @@ class APriori:
 
     def a_priori(self):
         fn = self.filename
-
         # Pass 1:
-        for bucket in self.selected_baskets:
+        for bucket in tqdm(self.selected_baskets, desc="Pass 1"):
             line = map(int, linecache.getline(fn, bucket).split())
             for i in line:
                 if self.max_int < i:
@@ -77,18 +79,30 @@ class APriori:
                 self.freq_item_table[i] = 0
 
         # Pass 2:
-        self.set_freq_item_table_length(int((self.max_int * (self.max_int-1))/2))
-        for bucket in self.selected_baskets:
-            print("BUCKET: {}".format(bucket))
-            line = [l for l in map(int, linecache.getline(fn, bucket).split())]
-            for i in line:
-                print(i, end=" ")
-            else:
-                break
+        tri_mat_size = int((self.max_int * (self.max_int-1))/2) + 1
+        self.set_pairs_tri_matrix_length(tri_mat_size)
+        for bucket in tqdm(self.selected_baskets, desc="Pass 2"):
+            line = list([l for l in map(int, linecache.getline(fn, bucket).split())])
+            if len(line) > 1:
+                for i in range(0, len(line)):
+                    if (self.freq_item_table[line[i]] != 0) and (i != len(line)-1):
+                        for j in range(i+1, len(line)):
+                            if self.freq_item_table[line[j]] != 0:
+                                k = line[i]
+                                m = line[j]
+                                index = int((k-1)*(self.max_int-(k/2)))+m-k
+                                self.pairs_tri_matrix[index] += 1
 
-
-
-
+        # identifying frequent pairs and print them
+        for i in range(1, self.max_int-1):
+            for j in range(i+1, self.max_int):
+                index = int((i-1)*(self.max_int-(i/2)))+j-i
+                if self.pairs_tri_matrix[index] < self.s:
+                    self.pairs_tri_matrix[i] = 0
+                else:
+                    if not self.freq_pairs_list.__contains__((i, j)):
+                        self.freq_pairs_list.append((i, j))
+                        print("{}, {} : {}".format(i, j, self.pairs_tri_matrix[index]))
 
 
 # Press the green button in the gutter to run the script.
